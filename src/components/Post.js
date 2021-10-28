83fa4b8cc19df1a9fed71715b3e04eb7b892993b
Aleksei <sinemettu@gmail.com>
2021-10-28 07:12:51 +0100

    Tags added to the post

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,9 +6,11 @@ import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export default function RecipeReviewCard({ post }) {
-  const { image, owner, text, likes } = post;
+  const { image, owner, text, likes, tags } = post;
   const formatedDate = new Date(Date.parse(post.publishDate))
     .toISOString()
     .slice(0, 10);
@@ -30,9 +32,16 @@ export default function RecipeReviewCard({ post }) {
           {text}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
+      <CardActions spacing={1}>
         <FavoriteIcon />
         <span style={{ marginLeft: '5px' }}>{likes}</span>
+      </CardActions>
+      <CardActions spacing={1}>
+        <Stack direction='row' spacing={1}>
+          {tags.map((tag) => (
+            <Chip key={tag} label={tag} variant='outlined' />
+          ))}
+        </Stack>
       </CardActions>
     </Card>
   );
